da9180b166afae9bfc90926f51b39b72f6213187
Samu Lang <langsamu@users.noreply.github.com>
2026-04-10 13:16:25 +0100

    Bump @rdfjs/wrapper to 0.33.0 (#4)

--- src/model/Item.ts
+++ src/model/Item.ts
@@ -1,4 +1,12 @@
-import { LiteralAs, LiteralFrom, NamedNodeAs, NamedNodeFrom, TermWrapper } from "@rdfjs/wrapper";
+import {
+    LiteralAs,
+    LiteralFrom,
+    NamedNodeAs,
+    NamedNodeFrom,
+    OptionalAs,
+    OptionalFrom,
+    TermWrapper
+} from "@rdfjs/wrapper";
 
 export class Item extends TermWrapper {
     get id(): string {
@@ -6,42 +14,42 @@ export class Item extends TermWrapper {
     }
 
     get thumbnail(): string | undefined {
-        return this.singularNullable("urn:example:/thumbnail", NamedNodeAs.string);
+        return OptionalFrom.subjectPredicate(this, "urn:example:/thumbnail", NamedNodeAs.string);
     }
 
     set thumbnail(value: string | undefined) {
-        this.overwriteNullable("urn:example:/thumbnail", value, NamedNodeFrom.string);
+        OptionalAs.object(this, "urn:example:/thumbnail", value, NamedNodeFrom.string);
     }
 
     get name(): string | undefined {
-        return this.singularNullable("urn:example:/name", LiteralAs.string);
+        return OptionalFrom.subjectPredicate(this, "urn:example:/name", LiteralAs.string);
     }
 
     set name(value: string | undefined) {
-        this.overwriteNullable("urn:example:/name", value, LiteralFrom.string);
+        OptionalAs.object(this, "urn:example:/name", value, LiteralFrom.string);
     }
 
     get featured(): boolean | undefined {
-        return this.singularNullable("urn:example:/featured", LiteralAs.boolean);
+        return OptionalFrom.subjectPredicate(this, "urn:example:/featured", LiteralAs.boolean);
     }
 
     set featured(value: boolean | undefined) {
-        this.overwriteNullable("urn:example:/featured", value, LiteralFrom.boolean);
+        OptionalAs.object(this, "urn:example:/featured", value, LiteralFrom.boolean);
     }
 
     get description(): string | undefined {
-        return this.singularNullable("urn:example:/description", LiteralAs.string);
+        return OptionalFrom.subjectPredicate(this, "urn:example:/description", LiteralAs.string);
     }
 
     set description(value: string | undefined) {
-        this.overwriteNullable("urn:example:/description", value, LiteralFrom.string);
+        OptionalAs.object(this, "urn:example:/description", value, LiteralFrom.string);
     }
 
     get website(): string | undefined {
-        return this.singularNullable("urn:example:/website", NamedNodeAs.string);
+        return OptionalFrom.subjectPredicate(this, "urn:example:/website", NamedNodeAs.string);
     }
 
     set website(value: string | undefined) {
-        this.overwriteNullable("urn:example:/website", value, NamedNodeFrom.string);
+        OptionalAs.object(this, "urn:example:/website", value, NamedNodeFrom.string);
     }
 }
--- src/model/List.ts
+++ src/model/List.ts
@@ -1,8 +1,8 @@
 import { Item } from "./Item";
-import { TermAs, TermFrom, TermWrapper } from "@rdfjs/wrapper";
+import { SetFrom, TermAs, TermFrom, TermWrapper } from "@rdfjs/wrapper";
 
 export class List extends TermWrapper {
     get item(): Set<Item> {
-        return this.objects("urn:example:/item", TermAs.instance(Item), TermFrom.instance);
+        return SetFrom.subjectPredicate(this, "urn:example:/item", TermAs.instance(Item), TermFrom.instance);
     }
 }
